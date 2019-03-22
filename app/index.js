e2e13923034fa8c7db0daf81b99386f33fd1fe66
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-03-23 05:03:47 +0800

    up

--- app/index.js
+++ app/index.js
@@ -1,9 +1,17 @@
 let initGantt = ()=>{
     let html = `<tr>`;
+    let dateMap = [];
+    let firstmom = moment('2019-01-01T00:00:00');
+    firstmom.subtract(1, 'days');
+    for(let j=0;j<365;j++){
+        let mom = firstmom.add(1, 'days');
+        dateMap.push(mom.format('MM-DD'))
+    }
+    //console.log(dateMap)
     for(let i=0;i<20;i++){
         html += `<td>${i+1}</td>`
-        for(let j=0;j<365;j++){
-            html += `<td>${j+1}</td>`
+        for(let j=0;j<dateMap.length;j++){
+            html += `<td>${dateMap[j]}</td>`
         }
         html += `</tr><tr>`
     }
