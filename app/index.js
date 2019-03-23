c331510210905b3961101b3cde7ea78b07bbe844
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-03-23 16:46:11 +0800

    up

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 let initGantt = ()=>{
     let html = `<tr>`;
-    let dateMap = [];
+    let canvasDateList = [];
+    let canvasDateInfo = {}
     let firstmom = moment('2019-01-01T00:00:00');
     firstmom.subtract(1, 'days');
 
@@ -9,13 +10,28 @@ let initGantt = ()=>{
     for(let j=0;j<365;j++){
         headhtml += `<th>${j+1}</th>`
         let mom = firstmom.add(1, 'days');
-        dateMap.push(mom.format('MM-DD'))
+        let dateId = 'id_'+mom.format('YYYYMMDD')
+        let info = {
+            month: mom.month(),
+            isToday:mom.isSame(moment(), 'day'),
+            isWeekend: (mom.day()===6 || mom.day()===0 || mom.day()===7)
+        }
+        let dateText = mom.format('MM.DD');
+        if(info.month<10) dateText=dateText.replace(/^0/,'')
+        
+        canvasDateInfo[dateId] = info;
+        canvasDateList.push({dateId, dateText});
     }
-    //console.log(dateMap)
+    //console.log(canvasDateList)
     for(let i=0;i<10;i++){
+        let dateinfo, dateid;
         html += `<td>${i+1}</td>`
-        for(let j=0;j<dateMap.length;j++){
-            html += `<td>${dateMap[j]}</td>`
+        for(let j=0;j<canvasDateList.length;j++){
+            dateid = canvasDateList[j].dateId;
+            dateinfo = canvasDateInfo[dateid]
+            html += `<td id="${canvasDateList[j].dateId}"
+                        class="${dateinfo.isToday?'today':''} ${dateinfo.isWeekend?'weekend':''}"
+                        >${canvasDateList[j].dateText}</td>`
         }
         html += `</tr><tr>`
     }
