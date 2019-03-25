8d5de140be28ea4b942e4bb89d6997c58a2d971e
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-03-26 01:39:37 +0800

    up

--- app/js/events.js
+++ app/js/events.js
@@ -2,8 +2,11 @@ let handleTdClick=(td)=>{
     //td=$(td);
     let tdid = td.getAttribute('id')
     let rowid = td.getAttribute('rowidx');
-    let tddate = td.getAttribute('date')
-    console.log(tdid, rowid, tddate)//)
+    let tddate = td.getAttribute('date');
+
+    $('tr>td').removeClass('selected_day')
+    $('#'+tdid).addClass('selected_day')
+    console.log(tdid, rowid, tddate, td.className)//)
     showDateInfo(tdid, rowid, tddate)
 }
 
