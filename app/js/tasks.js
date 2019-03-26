109dcd03477c4c55fa6e490ba7421190b25d7f78
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-03-27 01:56:17 +0800

    up

--- app/js/tasks.js
+++ app/js/tasks.js
@@ -19,26 +19,28 @@ let g_TaskData = [
     }
 ]
 
-let createTask=(rowId, startDate, task)=>{
-    let dateId = `${rowId}_d_${startDate}`//r1_d_2019-03-25
+let createOneTask=(rowId, startDate, task)=>{
+    let dateId = `${rowId}_${startDate}`//r1_d_2019-03-25
     let dateCell = document.getElementById(dateId)
     let days = task.days;
 
     let pos = $(dateCell).offset();
-    //console.log('pos',pos, dateCell)
+    //console.log('pos',pos, dateCell,dateId)
     let taskid = 'task'+(Math.random()+'').replace(/^0\./,'')
     let taskHtml = `<div id="${taskid}" class="task" style="left:${pos.left}px;top:${pos.top}px;">${task.subject}</div>`
 
     $('#tasks').append(taskHtml)
+    let taskobj = $(`#tasks>div[id="${taskid}"]`)
+    taskobj.css({left: pos.left - taskobj.outerWidth()})
 }
 let test = ()=>{
     let t0=new Date()
     for(let i=0;i<200;i++) {
         let date = moment().add(i, 'days').format('YYYY-MM-DD')
-        let j=i%9;
-        createTask('r'+j, date, {
-            subject: Math.random()+''+Math.random()
+        let j=i%6;
+        createOneTask('r'+j, date, {
+            subject: 'aaa'
         })
     }
-    console.log(new Date() - t0)
+    //console.log(new Date() - t0)
 }
