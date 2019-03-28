b52aebc053c9be24f15ecf2dfc6f4c8db7e0d836
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-03-29 04:49:19 +0800

    up

--- app/js/tasks.js
+++ app/js/tasks.js
@@ -17,7 +17,7 @@ let createTask=(data)=>{
     let pos = $(dateCell).offset();
     console.log('pos',pos, !!dateCell,dateId)
     let taskHtml = `<div id="${taskId}" class="task" style="right:${0}px;top:${0}px;">
-                        <pre>${subject}</pre>
+                        <pre><a href="javascript:void(0)" class="delete">X</a>|${subject}&gt;</pre>
                     </div>`
 
     //$('#tasks').append(taskHtml)
