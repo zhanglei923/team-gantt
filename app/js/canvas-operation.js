cc0de9d4b5850b8a5b7b13f695bfa14d22bc9a23
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-04-02 02:11:10 +0800

    up

--- app/js/canvas-operation.js
+++ app/js/canvas-operation.js
@@ -18,9 +18,30 @@ let g_choosingDates=[]
 let resetChooseDay=()=>{
     g_choosingRowIdx=null;
     g_choosingDates=[];
-    $('td').removeClass('choosed_day').html('')
+    g_chooseDate1=null;
+    $('td').removeClass('choosed_day').removeClass('choosed_day1').removeClass('choosed_day2')
+    $('tr>td>.diffdays').remove()
 }
+let g_chooseDate1;
 let chooseDay=(rowidx, date, td)=>{
+    if(!g_chooseDate1){
+        g_choosingRowIdx = rowidx;
+        g_chooseDate1 = date;
+        td.addClass('choosed_day1') 
+    }else{
+        let d1 = moment(g_chooseDate1);
+        let d2 = moment(date);
+        $('td').removeClass('choosed_day2');
+        let rowtd = getDayElem(g_choosingRowIdx, date)
+        rowtd.addClass('choosed_day2') 
+        if(d2.isAfter(d1)){
+            showDaysInbetween(g_choosingRowIdx, d1, d2)
+        }else{
+            $('td').removeClass('choosed_day2');
+        }
+    }
+}
+let chooseDay2=(rowidx, date, td)=>{
     let datemom = moment(date);
     if(g_choosingDates.length===0) g_choosingRowIdx = rowidx;
     let ok = true;
@@ -49,12 +70,12 @@ let unChooseDay=(rowidx, date)=>{
 
 }
 let showDaysInbetween=(rowidx, startDate, endDate)=>{
-    $('tr>td').html('')
-    let startMom = moment(startDate+'T00:00:00');
-    let endMom = moment(endDate+'T00:00:00');
+    $('tr>td>.diffdays').remove()
+    let startMom = moment(startDate);
+    let endMom = moment(endDate);
     if(!endMom.isAfter(startMom)) return;
-    let countDays=0;
-    let countWorkingDays=0;
+    let countDays=1;
+    let countWorkingDays=1;
     for(let i=0;i<1000;i++){
         startMom.add(1, 'days');
         let daytxt = startMom.format('YYYY-MM-DD')
@@ -69,7 +90,7 @@ let showDaysInbetween=(rowidx, startDate, endDate)=>{
             break;
         }
         let td=getDayElem(rowidx, daytxt);
-        if(td)td.html(`${countDays}<br>${countWorkingDays}`)
+        if(td)td.html(`<div class="diffdays">${countDays}<br>${countWorkingDays}</div>`)
         //console.log(startMom.format('YYYY-MM-DD'))
     }
 
