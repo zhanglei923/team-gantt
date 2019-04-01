3027e77b27c5636cc1015baea2064e52e08d2beb
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-04-01 22:50:00 +0800

    up

--- app/js/def_holidays.js
+++ app/js/def_holidays.js
@@ -6,21 +6,38 @@ let test_holidays = [
     {startDate: '2019-10-01', duration: 7, alteredWorkingDays:['2019-09-29']},
     {startDate: '2019-12-31', duration: 3, alteredWorkingDays:[]},
 ];
+let g_sprintSchedules = {
+
+}
 let test_ImportantWorkingDay = {
     '2019-04-11': {desc:'1904腾讯云压测', level:'notice'},
-    '2019-04-23': {desc:'1904Staging.', level:'notice'},
-    '2019-04-30': {desc:'1904Api-test', level:'notice'},
+    '2019-04-23': {desc:'1904Staging', level:'notice'},
+    '2019-04-30': {desc:'1904ApiTest', level:'notice'},
     '2019-05-22': {desc:'1904腾讯云UAT', level:'notice'},
     '2019-05-25': {desc:'1904腾讯云', level:'notice'},
 
-    '2019-03-22': {desc:'1904Sp3', level:'important'},
-    '2019-04-04': {desc:'1904Sp4', level:'important'},
-    '2019-04-19': {desc:'1904Intg', level:'important'},
-    '2019-04-27': {desc:'1904Gray', level:'important'},
-    '2019-05-04': {desc:'1904沙盒', level:'important'},
-    '2019-05-11': {desc:'1904Prod', level:'important'},
-    '2019-05-24': {desc:'1907Sp1', level:'important'},
+    '2019-03-11': {desc:'1904Sp3', level:'important'},
+    '2019-03-22': {desc:'1904Sp3$', level:'important'},
+    '2019-03-25': {desc:'1904sp4', level:'important'},
+    '2019-04-04': {desc:'1904Sp4$', level:'important'},
+    '2019-04-08': {desc:'1904Intg', level:'important'},
+    '2019-04-19': {desc:'1904Intg$', level:'important'},
+    '2019-04-27': {desc:'1904Gray', level:'notice'},
+    '2019-05-04': {desc:'1904沙盒', level:'notice'},
+    '2019-05-11': {desc:'1904Prod', level:'notice'},
+    '2019-04-29': {desc:'1907Sp0', level:'important'},
+    '2019-05-10': {desc:'1907Sp0$', level:'important'},
+    '2019-05-19': {desc:'1904大企业pod', level:'notice'},
+    '2019-05-13': {desc:'1907Sp1', level:'important'},
+    '2019-05-24': {desc:'1907Sp1$', level:'important'},
+    '2019-05-27': {desc:'1907Sp2', level:'important'},
+    '2019-06-06': {desc:'1907Sp2$', level:'important'},
+    '2019-06-10': {desc:'1907Sp3', level:'important'},
+    '2019-06-21': {desc:'1907Sp3$', level:'important'},
+    '2019-06-24': {desc:'1907Sp4', level:'important'},
+    '2019-07-05': {desc:'1907Sp4$', level:'important'},
 };
+Object.assign(test_ImportantWorkingDay, g_sprintSchedules)
 let test_TaskData = {
     'task_1': {
         rowIdx: 0,
