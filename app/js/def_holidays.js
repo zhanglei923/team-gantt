669feb7869f29a113bffc79160e6b62b0107d00d
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-04-04 23:46:42 +0800

    up

--- app/js/def_holidays.js
+++ app/js/def_holidays.js
@@ -7,16 +7,16 @@ let test_holidays = [
     {startDate: '2019-12-31', duration: 3, alteredWorkingDays:[]},
 ];
 let g_sprintSchedules = {
+    '2019-04-08': [{desc:'1904腾讯云压测', level:'notice'}],
     '2019-04-23': [{desc:'1904Staging', level:'notice'}],
-    '2019-04-28': [{desc:'1904腾讯云UAT', level:'notice'}],
+    '2019-04-27': [{desc:'1904Gray', level:'notice'}],
+    '2019-04-28': [{desc:'1904腾讯云UAT', level:'notice', offset_y:1}],
     '2019-04-30': [{desc:'1904ApiTest', level:'notice', offset_y:2}],
-    '2019-05-22': [{desc:'1904腾讯云UAT', level:'notice', offset_y:1}],
-    '2019-05-25': [{desc:'1904腾讯云', level:'notice'}],
-    '2019-04-27': [{desc:'1904Gray', level:'notice', offset_y:1}],
     '2019-05-04': [{desc:'1904沙盒', level:'notice'}],
     '2019-05-11': [{desc:'1904Prod', level:'notice'}],
-    '2019-04-08': [{desc:'1904腾讯云压测', level:'notice'}],
     '2019-05-19': [{desc:'1904大企业pod', level:'notice'}],
+    '2019-05-22': [{desc:'1904腾讯云UAT', level:'notice', offset_y:1}],
+    '2019-05-25': [{desc:'1904腾讯云', level:'notice'}],
 }
 let test_ImportantWorkingDay = {
     '2019-03-11': [{desc:'1904Sp3', level:'important'}],
