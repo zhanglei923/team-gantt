787e25423303a5491aebd6436f731133b518733e
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2020-01-15 06:20:13 +0800

    up

--- app/define/def_lanes.js
+++ app/define/def_lanes.js
@@ -11,22 +11,22 @@ let g_lanes_type = {
     },
     "zhanglei":{
         className: 'lane_1',
-        icon:'xxx://wiki.ingag'+'eapp.com/download/thum'+'bnails/28967268/%E4%BC%81%E4%B8%9A%E'+'5%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20190918141245.png?version=1&modificationDate=1568787219000&api=v2',
+        iconxxx:'https://wiki.ingag'+'eapp.com/download/thum'+'bnails/28967268/%E4%BC%81%E4%B8%9A%E'+'5%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20190918141245.png?version=1&modificationDate=1568787219000&api=v2',
         title:'磊'
     },
     "chengzhe":{
         className: 'lane_2',
-        icon: 'xxx://wiki.ingag'+'eapp.com/download/thum'+'bnails/28967268/%E4%BC%81%'+'E4%B8%9A%E5%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20190918141450.png?version=1&modificationDate=1568787341000&api=v2',
+        iconxxx: 'https://wiki.ingag'+'eapp.com/download/thum'+'bnails/28967268/%E4%BC%81%'+'E4%B8%9A%E5%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20190918141450.png?version=1&modificationDate=1568787341000&api=v2',
         title:'哲'
     },
     "liuhd":{
         className: 'lane_1',
-        icon: 'xxx://wiki.ingag'+'eapp.com/download/thumb'+'nails/28967268/%E4%BC%81%E4%B8%9A%E5'+'%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20190918141545.png?version=1&modificationDate=1568787389000&api=v2',
+        iconxxx: 'https://wiki.ingag'+'eapp.com/download/thumb'+'nails/28967268/%E4%BC%81%E4%B8%9A%E5'+'%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20190918141545.png?version=1&modificationDate=1568787389000&api=v2',
         title:'迪'
     },
     "sunxf":{
         className: 'lane_2',
-        icon: 'xxx://wiki.ingag'+'eapp.com/download/thumb'+'nails/28967268/%E4%BC%81%E4%B8%9A%E5'+'%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20190918141545.png?version=1&modificationDate=1568787389000&api=v2',
+        iconxxx: 'https://wiki.ingag'+'eapp.com/download/thumb'+'nails/28967268/%E4%BC%81%E4%B8%9A%E5'+'%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20190918141545.png?version=1&modificationDate=1568787389000&api=v2',
         title:'峰'
     }
 }
